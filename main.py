4d6f7d1b4201647d576380297315c80804e99011
klaus-moser <klaus-moser@posteo.de>
2022-05-08 21:46:30 +0200

    Exceptions and translation of every cell

--- main.py
+++ main.py
@@ -1,109 +1,41 @@
-import csv
-from os import environ, getcwd
-from os.path import join, exists
-from deepl import Translator, exceptions
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+""" CSV-Translator with deepl.com """
+
+
+__version__ = "1.0"
+__author__ = "github.com/klaus-moser"
+
+
+from csv_class import TranslateCsv
+from os import getcwd
 from dotenv import load_dotenv
-from tqdm import tqdm
 from tkinter import Tk, filedialog
 
-load_dotenv()
+
+load_dotenv()   # Load .env environment variables
 
 
-class TranslateCsv:
-    """Super Class."""
+def main(file):
+    """Main app."""
 
-    def __init__(self, file):
-        self.FILE_IN = self.check_file(file=file)
-        self.FILE_OUT = self.new_file_path()
-        self.auth_key = environ.get('AUTH_KEY')
-        self.translator = self.create_translator()
-        self.headers = None
-        self.text = None
-        self.text_translated = None
-
-    @staticmethod
-    def check_file(file):
-        """Check if file exists & is a .csv."""
-
-        if not exists(file) or not file.endswith('.csv'):
-            print("File not found / no .csv!")
-            exit()
-        else:
-            return file
-
-    def new_file_path(self):
-        """Creates a new file path for the translated file."""
-
-        temp = self.FILE_IN.split('.')
-        return join(temp[0] + '_translated.csv')
-
-    def create_translator(self):
-        """Create a translator object."""
-
-        try:
-            return Translator(auth_key=self.auth_key)
-        except Exception as e:
-            print(e)
-            exit()
-
-    def translate_text(self):
-        """Translate text with the deepL-API."""
-        try:
-            result = self.translator.translate_text(text=self.text, target_lang="DE")
-            result_clean = result.text.replace(',', '')
-            self.text_translated = result_clean
-
-        except exceptions.QuotaExceededException as e:
-            print("Limit exceeded!")
-            return False
-
-    def translate_csv(self):
-        """Open .csv and translate line by line."""
-
-        with open(file=self.FILE_IN, mode='r', encoding='utf-8') as r,\
-                open(file=self.FILE_OUT, mode='w', newline='', encoding='utf-8') as w:
-            r_csv = csv.reader(r)
-            w_csv = csv.writer(w)
-
-            if self.headers is None:
-                self.headers = next(r_csv)
-
-            w_csv.writerow(self.headers)
-
-            for row in tqdm(r_csv):
-                temp = row[1:]
-                self.text = ' '.join(temp)
-                if not self.translate_text():
-                    break
-                row = [row[0], self.text_translated]
-                w_csv.writerow(row)
-
-    def deepl_limit_exceeded(self):
-        """Check deepl account for its limit."""
-
-        usage = self.translator.get_usage()
-        if usage.character.limit_exceeded:
-            return True
-        else:
-            return False
-
-    def print_limit(self):
-        """Print deepL limits."""
-
-        usage = self.translator.get_usage()
-        print("Usage:", usage.character)
+    print("\n" + 10 * "*" + " CSV-Translator " + 10 * "*" + "\n")
+    translator = TranslateCsv(file=file)
+    translator.translate_csv()
 
 
 if __name__ == "__main__":
-    print("\n" + 10*"* " + "CSV-Translator" + 10*" *" + "\n")
-
     root = Tk()
     root.withdraw()
     FILE = filedialog.askopenfilename(filetypes=(("csv files", "*.csv"), ("csv files", "*.CSV")),
                                       parent=root,
                                       initialdir=getcwd(),
                                       title='Choose .csv:')
-
-    c = TranslateCsv(FILE)
-    c.print_limit()
-    c.translate_csv()
+    main(FILE)
---
+++ src/csv_class.py
@@ -0,0 +1,117 @@
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+import csv
+import deepl.exceptions
+from os import environ
+from os.path import join, exists
+from deepl import Translator, exceptions
+from tqdm import tqdm
+from exceptions import TranslationException
+
+
+class TranslateCsv:
+    """CSV Translator class."""
+
+    def __init__(self, file):
+        self.FILE_IN = self.check_file(file=file)
+        self.FILE_OUT = self.new_file_path()
+        self.translator = self.create_translator()
+        self.headers = None
+        self.text = None
+        self.text_translated = None
+        self.limit = 0
+
+    @staticmethod
+    def check_file(file):
+        """Check if file exists & is a .csv."""
+
+        if exists(file) and file.endswith('.csv'):
+            return file
+        else:
+            print("File not found / no .csv!")
+            exit()
+
+    def new_file_path(self):
+        """Creates a new file path for the translated file."""
+
+        temp = self.FILE_IN.split('.')
+        return join(temp[0] + '_translated.csv')
+
+    @staticmethod
+    def create_translator():
+        """Create a translator object."""
+
+        try:
+            return Translator(auth_key=environ.get('AUTH_KEY'))
+        except deepl.exceptions.AuthorizationException as e:
+            print("Authorization failed!")
+            exit()
+
+    def translate_text(self, target_lang="DE"):
+        """Translate text with the deepL-API."""
+
+        try:
+            self.text_translated = self.translator.translate_text(text=self.text, target_lang=target_lang)
+
+        except exceptions.QuotaExceededException as e:
+            print("Limit exceeded!")
+            return False
+
+    def clean_text(self, letter=','):
+        """Remove chosen letters."""
+
+        self.text_translated = self.text_translated.replace(letter, '')
+
+    def translate_csv(self):
+        """Open .csv and translate line by line."""
+
+        with open(file=self.FILE_IN, mode='r', encoding='utf-8') as r, \
+                open(file=self.FILE_OUT, mode='w', newline='', encoding='utf-8') as w:
+            r_csv = csv.reader(r)
+            w_csv = csv.writer(w)
+
+            if self.headers is None:
+                self.headers = next(r_csv)
+
+            w_csv.writerow(self.headers)
+
+            try:
+                for row in tqdm(r_csv):
+                    temp = []
+                    for column in row:
+                        self.text = column
+
+                        if not self.translate_text():
+                            raise TranslationException
+
+                        self.text_translated = self.text
+                        temp.append(self.text_translated)
+                    w_csv.writerow(temp)
+
+            except TranslationException:
+                pass
+
+    def set_limit(self):
+        """Save the limit of the characters."""
+
+        self.limit = self.translator.get_usage()
+
+    def get_limit(self):
+        """Print deepL limits."""
+
+        usage = self.translator.get_usage()
+        print("Usage:", usage.character)
+
+    def check_limit(self):
+        """Check deepl account for its limit."""
+
+        usage = self.translator.get_usage()
+        if usage.character.limit_exceeded:
+            exit("Character limit reached!")
