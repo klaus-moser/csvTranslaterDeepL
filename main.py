d492bda6d8de21d91d67c783c053c6019d69a787
klaus-moser <klaus-moser@posteo.de>
2022-05-08 16:25:21 +0200

    Translator

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import csv
 from os import environ
 from deepl import Translator
 from dotenv import load_dotenv
 
 load_dotenv()
+FILE = "data/test_data.csv"
 
-auth_key = environ.get('AUTH_KEY')
 
-translator = Translator(auth_key=auth_key)
+def translate(text: str):
+    """Translate text with the deepL-API"""
 
-result = translator.translate_text("Hello World!", target_lang="DE")
+    auth_key = environ.get('AUTH_KEY')
 
-print(result)
+    translator = Translator(auth_key=auth_key)
+    result = translator.translate_text(text=text, target_lang="DE")
+    result_clean = result.replace(',', '')
+
+    return result_clean
+
+
